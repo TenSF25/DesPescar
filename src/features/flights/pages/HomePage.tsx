c727e6741505c4ae76f5bc30771583036d823618
TenSF25 <thiagofilippo2504@gmail.com>
2026-08-04 23:46:58 -0300

    Se mejoro el diseño del home, y se lo adapto para que sea responsive

--- src/features/flights/pages/HomePage.tsx
+++ src/features/flights/pages/HomePage.tsx
@@ -1,6 +1,6 @@
-import { OffersDay } from '../components/OffersDay';
-import { SearchFly } from '../components/SearchFly';
-import { TrendFly } from '../components/TrendFly';
+import { OffersDay } from '../components/Home/OffersDay';
+import { SearchFly } from '../components/Home/SearchFly';
+import { TrendFly } from '../components/Home/TrendFly';
 
 export const HomePage = () => {
   return (
