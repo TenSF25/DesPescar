b7aa7d838443da2517aed0a0e48deead8890dc99
TenSF25 <thiagofilippo2504@gmail.com>
2026-08-23 16:57:17 -0300

    Se agrego el nuevo modelo de avion, a la interfaz

--- src/features/bookings/pages/BookingLayout.tsx
+++ src/features/bookings/pages/BookingLayout.tsx
@@ -3,9 +3,11 @@ import { Steps } from '../components/Steps';
 
 export const BookingLayout = () => {
   return (
-    <main className="mt-10 mr-auto mb-10 ml-auto flex w-full max-w-312.5 flex-col items-center justify-center gap-8">
-      <Steps />
-      <div>
+    <main className="mt-10 mr-auto mb-10 ml-auto flex w-full max-w-312.5 flex-col items-center justify-center gap-20">
+      <div className="mt-20">
+        <Steps />
+      </div>
+      <div className="">
         <Outlet />
       </div>
     </main>
--- src/features/bookings/pages/SeatSelectionPage.tsx
+++ src/features/bookings/pages/SeatSelectionPage.tsx
@@ -1,3 +1,15 @@
+import { AirplaneCanvas } from '../components/Plane2/AirplaneCanvas';
+
+import { Resume } from '../components/reservations/Resume';
+
 export const SeatSelectionPage = () => {
-  return <main>Asientos</main>;
+  return (
+    <div className="mx-auto min-h-screen w-full">
+      <AirplaneCanvas></AirplaneCanvas>
+
+      <section className="fixed top-50 right-30 z-30 w-md">
+        <Resume />
+      </section>
+    </div>
+  );
 };
